c8d3377fcc64b68b9e34f911a76cc630727c4d75
Stephen Chau <chtzch0713@gmail.com>
2022-11-17 13:59:01 -0800

    S Block finished

--- Blocks3D.py
+++ Blocks3D.py
@@ -38,7 +38,8 @@ def Update(deltaTime):
     cube.Update(deltaTime)
     # IBlock.Update(deltaTime)
     # OBlock.Update(deltaTime)
-    TBlock.Update(deltaTime)
+    # TBlock.Update(deltaTime)
+    SBlock.Update(deltaTime)
     return True
 
 
@@ -48,7 +49,8 @@ def Render():
     # cube.Render()
     # IBlock.Render()
     # OBlock.Render()
-    TBlock.Render()
+    # TBlock.Render()
+    SBlock.Render()
     pygame.display.flip()
     pass
 
