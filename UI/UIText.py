f040fbb3160767b76218eb3565822cd1f6fdba15
thisisalexfok <thisisalexfok@gmail.com>
2022-11-17 15:52:01 -0800

    Updated UI text module

--- UI/UIText.py
+++ UI/UIText.py
@@ -6,13 +6,14 @@ import freetype as ft
 
 class UIText():
     def __init__(self, text = None):
-        face = ft.Face("Vera.ttf")
-        face.set_char_size(48*64)
+        self.face = ft.Face(r'C:\Windows\Fonts\arial.ttf')
+        self.face.set_char_size(48*64)
+        self.face.load_char('S')
+        #face.load_char('S', ft.FT_LOAD_FLAGS['FT_LOAD_DEFAULT'])
         # Add Stroke
         # stroker = ft.Stroker()
         # stroker.set(1, ft.FT_STROKER_LINECAPS['FT_STROKER_LINECAP_ROUND'], ft.FT_STROKER_LINEJOINS['FT_STROKER_LINEJOIN_ROUND'], 0)
         # override default load flags to avoid rendering the character during loading
-        face.load_char('S', ft.FT_LOAD_FLAGS['FT_LOAD_DEFAULT'])
         # initialize C FreeType Glyph object
         # glyph = ft.FT_Glyph()
         # # get every glyph from the face
@@ -25,8 +26,7 @@ class UIText():
         #     raise ft.FT_Exception(error)
         # # bitmapGlyph is the rendered glyph that we want
         # bitmapGlyph = glyph.to_bitmap(ft.FT_RENDER_MODES['FT_RENDER_MODE_NORMAL'], 0)
-        bitmap = face.glyph.bitmap
-        print (bitmap.buffer)
+        
 
         pass
 
@@ -35,5 +35,9 @@ class UIText():
         #super().Update(deltaTime)
 
     def Render(self, screen):
-        pass
+        # m = glGetDouble(GL_MODELVIEW_MATRIX)
+        # glRotatef(self.ang, *self.axis)
+        # glLoadMatrixf(m)
+        bitmap = self.face.glyph.bitmap
+        print (bitmap.buffer)
         #super().Render(screen)
